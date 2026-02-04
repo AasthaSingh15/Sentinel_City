c824839b8118a9645da775eb63c81b1b604efab4
pallavi <pallavi6315@gmail.com>
2026-02-04 20:42:44 +0530

    added changes in frontend for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
-  }
-});
+  preview: {
+    allowedHosts: ['sentinel-city-zsz1.onrender.com'],
+  },
+})
+
 
